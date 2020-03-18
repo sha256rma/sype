bc2292dd54a467212292d09e0d3f4552812c45dd
eramm805 <eram.manasia@gmail.com>
2020-03-17 23:22:10 -0400

    my temp changes

--- ios/sype/AppDelegate.m
+++ ios/sype/AppDelegate.m
@@ -10,6 +10,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <RNScreenshotDetector/RNScreenshotDetector.h>
 
 @implementation AppDelegate
 
@@ -27,6 +28,9 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  RNScreenshotDetector* screenshotDetector = [[RNScreenshotDetector alloc] init];
+  [screenshotDetector setupAndListen:rootView.bridge];
+
   return YES;
 }
 
