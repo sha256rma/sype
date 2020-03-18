4c996773ea83dc7e51dbd74184de57afeb0070fa
Kartikeya <kartikeyasharma04@gmail.com>
2020-03-18 01:06:09 -0400

    Android CD working

--- App.js
+++ App.js
@@ -1,46 +1,44 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import {StatusBar} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 
 import FeedScreen from '@screens/app/FeedScreen';
 import ChatScreen from '@screens/app/ChatScreen';
 import ProfileScreen from '@screens/app/ProfileScreen';
-import CameraScreen from '@screens/app/CameraScreen';
 
 const Tab = createMaterialTopTabNavigator();
 
 function App() {
-    return (
-        <NavigationContainer>
-            <StatusBar backgroundColor={'powderblue'} barStyle="light-content"/>
-            <Tab.Navigator
-                initialRouteName="Feed"
-                tabBarOptions={{
-                    activeTintColor: 'black',
-                    labelStyle: { fontSize: 12 },
-                    style: { backgroundColor: 'powderblue' },
-                }}
-            >
-                <Tab.Screen
-                    name="Chat"
-                    component={ChatScreen}
-                    options={{ tabBarLabel: 'Chat' }}
-                />
-                <Tab.Screen
-                    name="Feed"
-                    component={FeedScreen}
-                    options={{ tabBarLabel: 'Feed' }}
-                />
+  return (
+    <NavigationContainer>
+      <StatusBar backgroundColor={'powderblue'} barStyle="light-content" />
+      <Tab.Navigator
+        initialRouteName="Feed"
+        tabBarOptions={{
+          activeTintColor: 'black',
+          labelStyle: {fontSize: 12},
+          style: {backgroundColor: 'powderblue'},
+        }}>
+        <Tab.Screen
+          name="Chat"
+          component={ChatScreen}
+          options={{tabBarLabel: 'Chat'}}
+        />
+        <Tab.Screen
+          name="Feed"
+          component={FeedScreen}
+          options={{tabBarLabel: 'Feed'}}
+        />
 
-                <Tab.Screen
-                    name="Profile"
-                    component={ProfileScreen}
-                    options={{ tabBarLabel: 'Profile' }}
-                />
-            </Tab.Navigator>
-        </NavigationContainer>
-    );
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{tabBarLabel: 'Profile'}}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default App;
