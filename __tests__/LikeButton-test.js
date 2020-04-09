099f8802c46725b8c37bf1cb93e4073d62974ceb
karnowsky1 <kestime9015@bths.edu>
2020-04-09 15:25:12 -0400

    completing LikeButton unit-test

--- __tests__/LikeButton-test.js
+++ __tests__/LikeButton-test.js
@@ -3,37 +3,19 @@ import React from 'react';
 import LikeButton from '@screens/app/Components/LikeButton/LikeButton';
 import { IconButton } from 'react-native-paper';
 import renderer from 'react-test-renderer';
-// var likeButton = new LikeButton();
 
-test('renders correctly', () => {
+
+test('LikeButton snapshot test', () => {
   const tree = renderer.create(<LikeButton/>).toJSON();
   expect(tree).toMatchSnapshot();
 });
 
-// test('functions beign called correctly', () => {
-//   expect(likeButton.handleLikeClick()).toBeFalsy();
-// });
 
-function sum(a,b){
-  return a + b;
-}
-
-it('testing like button',() => {
-  const heart = 'heart';
-  const heartOutline = 'heart-outline'
+it('testing handleLikeClick button',() => {
   const wrapper = shallow(<LikeButton isLiked ={false} />);
   const instance = wrapper.instance();
-  const iconButton = wrapper.find(IconButton).at(0);
-  console.log(instance.state.isLiked);
-  iconButton.simulate('click');
-  console.log(instance.state.isLiked);
+  const previousLikedValue = instance.state.isLiked;
   instance.handleLikeClick();
-  console.log(instance.state.isLiked);
-  const previousIconValue = iconButton.props().icon;
-  const oppositeIconValue = (previousIconValue === heart) ? heartOutline : heart;
-  
-  console.log(iconButton.debug());
-  console.log(previousIconValue);
-  console.log(oppositeIconValue);
-  expect(sum(1,2)).toEqual(3);
+  const currentLikedValue = instance.state.isLiked;
+  expect(currentLikedValue).toEqual(!previousLikedValue);
 });
