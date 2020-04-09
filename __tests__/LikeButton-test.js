5cd2ea0398970432193b37c8704a8e1a2f81421c
karnowsky1 <kestime9015@bths.edu>
2020-04-09 19:35:11 -0400

    adjusting indentation

--- __tests__/LikeButton-test.js
+++ __tests__/LikeButton-test.js
@@ -2,14 +2,15 @@ import 'react-native';
 import React from 'react';
 import LikeButton from '@screens/app/Components/LikeButton/LikeButton';
 import renderer from 'react-test-renderer';
+import {shallow} from 'enzyme';
 
 test('LikeButton snapshot test', () => {
-  const tree = renderer.create(<LikeButton/>).toJSON();
+  const tree = renderer.create(<LikeButton />).toJSON();
   expect(tree).toMatchSnapshot();
 });
 
-it('testing handleLikeClick function',() => {
-  const wrapper = shallow(<LikeButton isLiked ={false} />);
+it('testing handleLikeClick function', () => {
+  const wrapper = shallow(<LikeButton isLiked={false} />);
   const instance = wrapper.instance();
   const previousLikedValue = instance.state.isLiked;
   instance.handleLikeClick();
