370e7e5be4121af2db59c2c08dfe23bee87eeb7c
karnowsky1 <kestime9015@bths.edu>
2020-04-15 10:11:33 -0400

    adding LikeButton heart tests

--- __tests__/LikeButton-test.js
+++ __tests__/LikeButton-test.js
@@ -3,6 +3,7 @@ import React from 'react';
 import LikeButton from '@screens/app/Components/LikeButton/LikeButton';
 import renderer from 'react-test-renderer';
 import {shallow} from 'enzyme';
+import {IconButton} from 'react-native-paper';
 
 test('like button snapshot test', () => {
   const tree = renderer.create(<LikeButton />).toJSON();
@@ -24,4 +25,21 @@ function testHandleLikeClick(initialLikeStatus) {
 describe('testing handeLikeClick cases', () => {
   testHandleLikeClick(true);
   testHandleLikeClick(false);
+});
+
+describe('testing icon button heart values', () => {
+  const heart = 'heart';
+  const heartOutline = 'heart-outline';
+  const wrapper = shallow(<LikeButton isLiked={false} />);
+  const iconButton = wrapper.find(IconButton).at(0);
+  it('heart outlined when isLiked is false', () => {
+    const heartValue = iconButton.props().icon;
+    expect(heartValue).toEqual(heartOutline);
+  });
+  const newWrapper = shallow(<LikeButton isLiked={true} />);
+  const newIconButton = newWrapper.find(IconButton).at(0);
+  it('heart filled when isLiked is true', () => {
+    const heartValue = newIconButton.props().icon;
+    expect(heartValue).toEqual(heart);
+  });
 });
