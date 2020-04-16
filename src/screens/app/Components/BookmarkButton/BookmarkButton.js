6da38be08a2292fce193a41f7a232e2be7f64165
eramm805 <eram.manasia@gmail.com>
2020-04-15 23:33:23 -0400

    Fixing merge conflicts

--- src/screens/app/Components/BookmarkButton/BookmarkButton.js
+++ src/screens/app/Components/BookmarkButton/BookmarkButton.js
@@ -1,9 +1,5 @@
 import {IconButton} from 'react-native-paper';
 import React from 'react';
-<<<<<<< HEAD
-=======
-import styles from './styles';
->>>>>>> 36ab8c947c992f70038794805d31bae31b7c7a82
 
 export default class BookmarkButton extends React.Component {
   constructor(props) {
