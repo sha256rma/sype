3d0ebdd0783791d7cd899bbe9c10d36be9112993
eramm805 <eram.manasia@gmail.com>
2020-04-15 23:44:23 -0400

    Fixed merge conflicts and pass test now

--- src/screens/app/Components/SettingsButtonHorizontal/SettingsButtonHorizontal.js
+++ src/screens/app/Components/SettingsButtonHorizontal/SettingsButtonHorizontal.js
@@ -1,22 +1,12 @@
 import {IconButton} from 'react-native-paper';
 import React from 'react';
-<<<<<<< HEAD
-=======
-import styles from './styles';
->>>>>>> 36ab8c947c992f70038794805d31bae31b7c7a82
 
 export default class SettingsButtonHorizontal extends React.Component {
   state = {
     iconSize: 30,
     settingsButtonHorizontalIcon: 'dots-horizontal',
   };
-<<<<<<< HEAD
   handleSettingsClick = () => {};
-=======
-
-  handleSettingsClick = () => {};
-
->>>>>>> 36ab8c947c992f70038794805d31bae31b7c7a82
   render() {
     return (
       <IconButton
