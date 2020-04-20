dd8ffa0c1a95c389bf396b52e1512d11c8964a54
eramm805 <eram.manasia@gmail.com>
2020-04-20 19:18:13 -0400

    Fixed Linting Issues

--- src/screens/app/Components/ProfileBannerAvatar/ProfileBannerAvatar.js
+++ src/screens/app/Components/ProfileBannerAvatar/ProfileBannerAvatar.js
@@ -9,22 +9,21 @@ export default class ProfileBannerAvatar extends React.Component {
 
   render() {
     const profile_image = this.props.profile_image;
-    
+
     /** This is a varibale  */
     const profile_image_size = 75;
 
     return (
-
-    <View style={styles.profileBannerImageContent}>
+      <View style={styles.profileBannerImageContent}>
         <Avatar.Image size={profile_image_size} source={{uri: profile_image}} />
-    </View>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-    profileBannerImageContent: {
-        flexDirection: 'row', 
-        alignItems: 'center',
-    },
+  profileBannerImageContent: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
 });
