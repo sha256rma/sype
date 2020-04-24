d045839872aaeb3eb5add5ac7cdc3da3ca2e7698
bujars <bujarsefa9@gmail.com>
2020-04-23 22:17:58 -0400

    Stashing commit changes

--- src/screens/Feed.js
+++ src/screens/Feed.js
@@ -6,9 +6,9 @@ import {
   FlatList,
   TouchableWithoutFeedback,
 } from 'react-native';
-import SettingsButtonHorizontal from './Components/SettingsButtonHorizontal/SettingsButtonHorizontal';
-import ProfileAvatar from './Components/ProfileAvatar/ProfileAvatar';
-import Post from './Components/Post/Post';
+import SettingsButtonHorizontal from './app/Components/SettingsButtonHorizontal/SettingsButtonHorizontal';
+import ProfileAvatar from './app/Components/ProfileAvatar/ProfileAvatar';
+import Post from './app/Components/Post/Post';
 export default class FeedScreen extends React.Component {
   constructor(props) {
     super(props);
