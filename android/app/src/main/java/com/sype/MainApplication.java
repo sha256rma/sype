3eb037238b4c4a73df37c2653f6363935e00a331
Denny Liang <denny.liang29@gmail.com>
2020-04-30 00:59:30 -0400

    camera installation

--- android/app/src/main/java/com/sype/MainApplication.java
+++ android/app/src/main/java/com/sype/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import org.reactnative.camera.RNCameraPackage;
 import com.swmansion.reanimated.ReanimatedPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
